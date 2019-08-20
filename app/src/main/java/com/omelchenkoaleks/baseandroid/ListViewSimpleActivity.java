57e71e8f767037859c70dd6cbc7548fd5b43f0d9
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-08-20 07:42:11 +0300

    заполняем список в отдельном методе ... )

--- app/src/main/java/com/omelchenkoaleks/baseandroid/ListViewSimpleActivity.java
+++ app/src/main/java/com/omelchenkoaleks/baseandroid/ListViewSimpleActivity.java
@@ -7,6 +7,9 @@ import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListViewSimpleActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -14,8 +17,26 @@ public class ListViewSimpleActivity extends Activity {
         setContentView(R.layout.activity_list_view_simple);
 
         ListView listView = findViewById(R.id.list_view);
-        String[] list = {"doctor", "dog", "method", "milk", "car", "work"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adapter);
+        List<String> items = getItems();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(arrayAdapter);
+
+        // 1 пример
+//        ListView listView = findViewById(R.id.list_view);
+//        String[] list = {"doctor", "dog", "method", "milk", "car", "work"};
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+//        listView.setAdapter(adapter);
+    }
+
+    private List<String> getItems() {
+        List<String> items = new ArrayList<>();
+
+        items.add("doctor");
+        items.add("dog");
+        items.add("method");
+        items.add("big");
+        items.add("small");
+
+        return items;
     }
 }
